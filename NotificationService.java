9d5a76c1bff5119ba82318f86de693cc69b56d34
Jahid Hasan <jahidcseru1998@gmail.com>
2022-08-31 14:19:31 +0600

    Updated factory pattern object

--- NotificationService.java
+++ NotificationService.java
@@ -4,12 +4,13 @@ public class NotificationService {
         NotificationFactory notificationFactory = new NotificationFactory();
         
         Notification notification = notificationFactory.createNotification("SMS");
-        notification.notifyUser();
+        String text="Hellow wordl";
+        notification.notifyUser(text);
 
         Notification notification2 = notificationFactory.createNotification("EMAIL");
-        notification2.notifyUser();
+        notification2.notifyUser(text);
 
         Notification notification3 = notificationFactory.createNotification("PUSH");
-        notification3.notifyUser();
+        notification3.notifyUser(text);
     }
 }
